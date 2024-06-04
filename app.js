da50fa356bd5494f4ccfdc52622793eab778bcc2
Filippo <122409227+FMonte03@users.noreply.github.com>
2024-06-04 18:11:58 -0400

    added Modal Menu with form, need to make it actually add the task

---
+++ app.js
@@ -0,0 +1,8 @@
+const addTask = document.querySelector('.addButton')
+const Modal = document.querySelector('.modal')
+addTask.addEventListener('click', ShowMenu )
+
+function ShowMenu(){
+ 
+    Modal.style.display = 'block'
+}
